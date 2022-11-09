39d5f58f13b0e816f6555661c99af957756ebc41
JakubJop <kuba_04042@interia.pl>
2022-11-09 18:40:07 +0100

    przyklad

--- PRogramowanie/main.py
+++ PRogramowanie/main.py
@@ -1,9 +1,4 @@
-n = int(input("Podaj liczbę studentów: "))
-n = 3
-suma = 0
-while True:
-    print("punkty")
-    x = input()
-    suma += int(x)
-    print(suma)
-    print(suma/3)
+a = int(input("Podaj liczbę kotów: "))
+b = int(input("Podaj liczbę psów: "))
+
+
