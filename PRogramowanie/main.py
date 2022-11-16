10972ea6a98998373edd9a4eed0d5069d3451045
JakubJop <kuba_04042@interia.pl>
2022-11-16 11:25:45 +0100

    przyklad1

--- PRogramowanie/main.py
+++ PRogramowanie/main.py
@@ -1,4 +1,7 @@
-a = int(input("Podaj liczbę kotów: "))
-b = int(input("Podaj liczbę psów: "))
-
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj drugą liczbę: "))
+suma = a + b
+iloczyn = a*b
+print("iloczyn: ", iloczyn)
+print("Suma: ",suma)
 
