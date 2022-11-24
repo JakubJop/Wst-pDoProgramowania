6bbb0bdefabedfc1898c9d33e39bf1b8bf0330b9
w67238 <kuba_04042@interia.pl>
2022-11-24 18:23:29 +0100

    lab51

--- Lab6.3.py
+++ Lab6.3.py
@@ -1,15 +1,13 @@
 
-def maximum(, *args):
-    if len(args) == 0:
-        return None
-    m = args[0]
-    for a in args[1:]:
+def maximum(a1, *args):
+    m = a1
+    for a in args:
         if m < a:
             m = a
     return a
 
-maks = maximum( 1, 3, 4, 10, 5)
+maks = maximum2( 1, 3, 4, 10, 5)
 print(maks)
 
-maks = maximum ()
+maks = maximum2(3)
 print(maks)
