bd8289e3fbb840060d08d783dad47b04c5141227
w67238 <kuba_04042@interia.pl>
2022-11-24 18:00:18 +0100

    lab5

---
+++ Lab6.2.py
@@ -0,0 +1,15 @@
+"def oblicz(liczba1, liczba2):
+   " x = liczba1 + liczba2
+  """"  y = liczba1 - liczba2"
+    return x,y
+
+f = oblicz(4,4)
+print(f[0],f[1])
+a,b = oblicz(4.5,5.4)
+print(a,b)"
+
+""""
+
+
+
+
---
+++ Lab6.3.py
@@ -0,0 +1,3 @@
+def z3(*krotka):
+    print(krotka)
+print(z3(2, 3, 4, 5, 7, 8, 6.39))
